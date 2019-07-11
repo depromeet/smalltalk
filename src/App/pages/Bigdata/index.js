a3426336386d8d46286bef9c40ff57c930a0ad3b
yongseongjeon <yongseong.jeon95@gmail.com>
2019-07-11 23:47:14 +0900

    통계페이지 나라 리스트 css 추가합니다.

--- src/App/pages/Bigdata/index.js
+++ src/App/pages/Bigdata/index.js
@@ -11,7 +11,9 @@ class Bigdata extends Component{
   render(){
     return(
         <div className={cx("bigdata")}>
+            {/* 스몰토크 로고 */}
             <div className="logo"></div>
+            {/* 검색 바 */}
             <div className={cx("search-bar")}>
                 <input className={cx("input")} placeholder="나라 / 도시 검색"></input>
                 <div className={cx("drop-down-button")}></div>
@@ -25,6 +27,23 @@ class Bigdata extends Component{
                     </div>
                 </div>
                 <div className={cx("arrow")}></div>
+            </div>
+            {/* 나라 리스트 */}
+            <div className={cx("country-list")}>
+                <div className={cx("box")}>
+                    <img src="https://cdn.zeplin.io/5cfc3a08cb970515fca66b80/assets/90CD9AE9-4B90-4787-89C4-0629DDE52E9F.png" className={cx("picture")} alt="country"></img>
+                    <div className={cx("left-row")}>
+                        <div className={cx("country-name")}>리스본</div>
+                        <div className={cx("content")}>
+                            <div className={cx("phrases")}>현재</div>
+                            <div className={cx("number")}>50명</div>
+                            <div className={cx("phrases")}>이 여행중입니다</div>
+                        </div>
+                    </div>
+                    <div className={cx("right-row")}>
+                        <div className={cx("text")}>바로보기</div>
+                    </div>
+                </div>
             </div>
             {/* 친구 리스트, 채팅 버튼 */}
             <div className="buttons">
