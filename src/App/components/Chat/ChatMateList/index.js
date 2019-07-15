62789ba02355b0dcd07077fbff7a90a2f2454f52
yongseongjeon <yongseong.jeon95@gmail.com>
2019-07-15 17:41:24 +0900

    메인페이지 친구목록 친구요청 열고 닫는 기능 추가합니다.

--- src/App/components/Chat/ChatMateList/index.js
+++ src/App/components/Chat/ChatMateList/index.js
@@ -3,11 +3,24 @@ import EachFriendsList from './EachFriendsList/index';
 import ChatMateRequest from '../ChatMateRequest/index';
 import classNames from 'classnames/bind';
 import styles from './style.module.scss';
+const cx = classNames.bind(styles);
 
 class ChatMateList extends Component{
+    constructor(props){
+    super(props);
+
+    this.state = {
+      isClickedFoldButton : false
+    }
+  }
+  handleFoldButton = () => {
+    this.setState({isClickedFoldButton : true})
+  }
+  handleMoreButton = () => {
+    this.setState({isClickedFoldButton : false})
+  }
 
   render(){
-    const cx = classNames.bind(styles);
       return(
         <div className={cx("my-mate")}>
           <div className={cx("buttons")}>
@@ -17,22 +30,45 @@ class ChatMateList extends Component{
             <div className={cx("list")}>
               <div className={cx("header")}>현재<br></br>내 메이트 {this.props.FriendsListDataLength}명</div>
               <div className={cx("close-button")} onClick={this.props.handleAllClose}></div>
+              {this.state.isClickedFoldButton ?
+              <div className={cx("collection-2")}>
+              {this.props.FriendsListData.map((list, i) => {
+                  return (<EachFriendsList 
+                              picture={list.picture}
+                              name={list.name}
+                              number={list.number}
+                              key={i}
+                              handleChatClick={this.props.handleChatClick}
+                              showRedCircle={false}
+                              handleChatRoomClick={this.props.handleChatRoomClick}
+                              handleName={this.props.handleName}
+                          />);
+              })}
+              </div>
+              :
               <div className={cx("collection")}>
-            {this.props.FriendsListData.map((list, i) => {
-                return (<EachFriendsList 
-                            picture={list.picture}
-                            name={list.name}
-                            number={list.number}
-                            key={i}
-                            handleChatClick={this.props.handleChatClick}
-                            showRedCircle={false}
-                            handleChatRoomClick={this.props.handleChatRoomClick}
-                            handleName={this.props.handleName}
-                        />);
-                    })}
-            </div>
+                {this.props.FriendsListData.map((list, i) => {
+                    return (<EachFriendsList 
+                                picture={list.picture}
+                                name={list.name}
+                                number={list.number}
+                                key={i}
+                                handleChatClick={this.props.handleChatClick}
+                                showRedCircle={false}
+                                handleChatRoomClick={this.props.handleChatRoomClick}
+                                handleName={this.props.handleName}
+                            />);
+                })}
+              </div>
+              }
           </div>
+          {this.state.isClickedFoldButton ? 
+          <div className={cx("apply-friend-2")}>
+            <div className={cx("rotated-fold-button")} onClick={this.handleMoreButton}></div>
+          </div>
+          :
           <div className={cx("apply-friend")}>
+            <div className={cx("fold-button")} onClick={this.handleFoldButton}></div>
             <div className={cx("collection")}>
             {this.props.ApplyListData.map((list, i) => {
                 return (<ChatMateRequest 
@@ -43,9 +79,9 @@ class ChatMateList extends Component{
                             denyMateRequest={this.props.denyMateRequest}
                         />);
                     })}
-              </div>
             </div>
-          </div>
+          </div>}
+        </div>
         )
   }
 }
