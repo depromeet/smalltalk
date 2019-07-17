48772fe3bcaf78b14ead2515678c7aec11ef1679
yongseongjeon <yongseong.jeon95@gmail.com>
2019-07-17 17:41:25 +0900

    Be searched city list 함수형 컴포넌트로 수정합니다.

--- src/App/components/Bigdata/BeSearchedCityList/index.js
+++ src/App/components/Bigdata/BeSearchedCityList/index.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classnames from 'classnames/bind';
 import styles from './style.module.scss';
 
 const cx = classnames.bind(styles);
 
-class CountryList extends Component {
-//   constructor(props){
-//     super(props);
-//   }
-
-  render() {
-    if (this.props.isClickedInputButton === true && this.props.input !== '') {
-      if (this.props.name.indexOf(this.props.input) === -1) return <div />;
-      if (this.props.name.indexOf(this.props.input) === 0) return <div className={cx('city-name')}>{this.props.name}</div>;
-      return <div />;
-    } return <div />;
-  }
+const CountryList = ( {name, input, isClickedInputButton} ) => {
+  if (isClickedInputButton === true && input !== '') {
+    if (name.indexOf(input) === -1) return <div />;
+    if (name.indexOf(input) === 0) return <div className={cx('city-name')}>{name}</div>;
+    return <div />;
+  } return <div />;
 }
 
 export default CountryList;
