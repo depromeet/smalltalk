ad90dcbaeb061a21da5ed6723754cbfb02d91d59
yongseongjeon <yongseong.jeon95@gmail.com>
2019-07-17 17:38:58 +0900

    List of people in the city 함수형 컴포넌트로 수정합니다.

--- src/App/components/Bigdata/ListOfPeopleInTheCity/index.js
+++ src/App/components/Bigdata/ListOfPeopleInTheCity/index.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classnames from 'classnames/bind';
 import styles from './style.module.scss';
 
 const cx = classnames.bind(styles);
 
-class ListOfPeopleInTheCity extends Component {
-//   constructor(props){
-//     super(props);
-//   }
-friendRequest = () => {
-  this.props.handleFriendRequest(this.props.id);
-}
+const ListOfPeopleInTheCity = ( {id, picture, nickname, age, gender, handleFriendRequest, isClickedFriendRequest} ) => {
 
-render() {
+  const friendRequest = () => {
+    handleFriendRequest(id);
+  }
+
   return (
     <div>
-      {this.props.isClickedFriendRequest
+      {isClickedFriendRequest
         ? (
           <div className={cx('box-2')}>
-            <img src={this.props.picture} className={cx('picture')} alt="profile" />
+            <img src={picture} className={cx('picture')} alt="profile" />
             <div className={cx('left-row')}>
-              <div className={cx('nickname')}>{this.props.nickname}</div>
+              <div className={cx('nickname')}>{nickname}</div>
               <div className={cx('content')}>
-                <div className={cx('age')}>{this.props.age}</div>
-                <div className={cx('gender')}>{this.props.gender}</div>
+                <div className={cx('age')}>{age}</div>
+                <div className={cx('gender')}>{gender}</div>
               </div>
             </div>
             <div className={cx('right-row')}>
@@ -33,23 +30,21 @@ render() {
         )
         : (
           <div className={cx('box')}>
-            <img src={this.props.picture} className={cx('picture')} alt="profile" />
+            <img src={picture} className={cx('picture')} alt="profile" />
             <div className={cx('left-row')}>
-              <div className={cx('nickname')}>{this.props.nickname}</div>
+              <div className={cx('nickname')}>{nickname}</div>
               <div className={cx('content')}>
-                <div className={cx('age')}>{this.props.age}</div>
-                <div className={cx('gender')}>{this.props.gender}</div>
+                <div className={cx('age')}>{age}</div>
+                <div className={cx('gender')}>{gender}</div>
               </div>
             </div>
-            <div className={cx('right-row')} onClick={this.friendRequest}>
+            <div className={cx('right-row')} onClick={friendRequest}>
               <div className={cx('text')}>친구신청</div>
             </div>
           </div>
-        )
-                }
+        )}
     </div>
   );
-}
 }
 
 export default ListOfPeopleInTheCity;
