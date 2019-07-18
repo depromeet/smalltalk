e12816802d4a43ee5f5696916dd5243f6b3ae2de
yongseongjeon <yongseong.jeon95@gmail.com>
2019-07-18 12:20:15 +0900

    ChatContent 컴포넌트의 TypeError 해결합니다, 함수형 컴포넌트로 수정합니다.

--- src/App/components/Chat/ChatContent/index.js
+++ src/App/components/Chat/ChatContent/index.js
@@ -4,42 +4,34 @@ import styles from './style.module.scss';
 
 const cx = classNames.bind(styles);
 
-class ChatContent extends Component {
-  render() {
-
-    // lastType을 정의하는데 TypeError가 떠서 임시로 try문 선언합니다. 수정해야합니다.
-    try {
-      var lastType = this.props.lastType.type;
-    } catch {
-      console.log('hi err');
-    }
-    return (
-      <div>
-        {/* 상대방 채팅일 경우 */}
-        {this.props.type === 'opposite'
-          ? (
-            <div className={cx('opposite')}>
-              {/* 이전 채팅의 type을 확인해서 상대방 채팅이었으면 공백을, 상대방 채팅이 아니면 프로필 사진을 보여줍니다. */}
-              {lastType === 'opposite' ? <div className={cx('space')} /> : <div className={cx('profile-picture')} />}
-              <div className={cx('talk-list')}>
-                <div className={cx('background')}>
-                  <div className={cx('sentence')}>{this.props.text}</div>
-                </div>
+const ChatContent = ( {lastType = '', type, text} ) => {
+  let lastChatType = lastType.type;
+  return (
+    <div>
+      {/* 상대방 채팅일 경우 */}
+      {type === 'opposite'
+        ? (
+          <div className={cx('opposite')}>
+            {/* 이전 채팅의 type을 확인해서 상대방 채팅이었으면 공백을, 상대방 채팅이 아니면 프로필 사진을 보여줍니다. */}
+            {lastChatType === 'opposite' ? <div className={cx('space')} /> : <div className={cx('profile-picture')} />}
+            <div className={cx('talk-list')}>
+              <div className={cx('background')}>
+                <div className={cx('sentence')}>{text}</div>
               </div>
             </div>
-          )
-          : (
-            <div className={cx('mine')}>
-              <div className={cx('talk-list')}>
-                <div className={cx('background')}>
-                  <div className={cx('sentence')}>{this.props.text}</div>
-                </div>
+          </div>
+        )
+        : (
+          <div className={cx('mine')}>
+            <div className={cx('talk-list')}>
+              <div className={cx('background')}>
+                <div className={cx('sentence')}>{text}</div>
               </div>
             </div>
-          )}
-      </div>
-    );
-  }
+          </div>
+        )}
+    </div>
+  );
 }
 
 export default ChatContent;
